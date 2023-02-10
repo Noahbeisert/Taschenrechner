d7f1c2afe584e64e028a9366ad812903ba5da269
E534550 <david.beisert@sbb.ch>
2023-02-10 18:16:27 +0100

    Erste Version

---
+++ Taschenrechner/TaschenrechnerForm.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Taschenrechner
+{
+    partial class TaschenrechnerForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textFuerZahlen = new System.Windows.Forms.TextBox();
+            this.zahl2 = new System.Windows.Forms.Button();
+            this.zahl1 = new System.Windows.Forms.Button();
+            this.zahl3 = new System.Windows.Forms.Button();
+            this.zahl4 = new System.Windows.Forms.Button();
+            this.zahl5 = new System.Windows.Forms.Button();
+            this.zahl6 = new System.Windows.Forms.Button();
+            this.zahl7 = new System.Windows.Forms.Button();
+            this.zahl8 = new System.Windows.Forms.Button();
+            this.zahl9 = new System.Windows.Forms.Button();
+            this.pluszeichen = new System.Windows.Forms.Button();
+            this.minuszeichen = new System.Windows.Forms.Button();
+            this.geteiltzeichen = new System.Windows.Forms.Button();
+            this.malzeichen = new System.Windows.Forms.Button();
+            this.resetknopf = new System.Windows.Forms.Button();
+            this.zahl0 = new System.Windows.Forms.Button();
+            this.gleichzeichen = new System.Windows.Forms.Button();
+            this.textFuerZwischenErgebnis = new System.Windows.Forms.TextBox();
+            this.labelFuerZwischenergebnis = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textFuerZahlen
+            // 
+            this.textFuerZahlen.Enabled = false;
+            this.textFuerZahlen.Location = new System.Drawing.Point(222, 39);
+            this.textFuerZahlen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textFuerZahlen.Name = "textFuerZahlen";
+            this.textFuerZahlen.Size = new System.Drawing.Size(270, 22);
+            this.textFuerZahlen.TabIndex = 1;
+            // 
+            // zahl2
+            // 
+            this.zahl2.Location = new System.Drawing.Point(222, 110);
+            this.zahl2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl2.Name = "zahl2";
+            this.zahl2.Size = new System.Drawing.Size(67, 34);
+            this.zahl2.TabIndex = 2;
+            this.zahl2.Text = "2";
+            this.zahl2.UseVisualStyleBackColor = true;
+            this.zahl2.Click += new System.EventHandler(this.zahl2_Click);
+            // 
+            // zahl1
+            // 
+            this.zahl1.Location = new System.Drawing.Point(139, 110);
+            this.zahl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl1.Name = "zahl1";
+            this.zahl1.Size = new System.Drawing.Size(67, 34);
+            this.zahl1.TabIndex = 3;
+            this.zahl1.Text = "1";
+            this.zahl1.UseVisualStyleBackColor = true;
+            this.zahl1.Click += new System.EventHandler(this.zahl1_Click);
+            // 
+            // zahl3
+            // 
+            this.zahl3.Location = new System.Drawing.Point(309, 110);
+            this.zahl3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl3.Name = "zahl3";
+            this.zahl3.Size = new System.Drawing.Size(67, 34);
+            this.zahl3.TabIndex = 4;
+            this.zahl3.Text = "3";
+            this.zahl3.UseVisualStyleBackColor = true;
+            this.zahl3.Click += new System.EventHandler(this.zahl3_Click);
+            // 
+            // zahl4
+            // 
+            this.zahl4.Location = new System.Drawing.Point(139, 161);
+            this.zahl4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl4.Name = "zahl4";
+            this.zahl4.Size = new System.Drawing.Size(67, 34);
+            this.zahl4.TabIndex = 5;
+            this.zahl4.Text = "4";
+            this.zahl4.UseVisualStyleBackColor = true;
+            this.zahl4.Click += new System.EventHandler(this.zahl4_Click);
+            // 
+            // zahl5
+            // 
+            this.zahl5.Location = new System.Drawing.Point(222, 161);
+            this.zahl5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl5.Name = "zahl5";
+            this.zahl5.Size = new System.Drawing.Size(67, 34);
+            this.zahl5.TabIndex = 6;
+            this.zahl5.Text = "5";
+            this.zahl5.UseVisualStyleBackColor = true;
+            this.zahl5.Click += new System.EventHandler(this.zahl5_Click);
+            // 
+            // zahl6
+            // 
+            this.zahl6.Location = new System.Drawing.Point(309, 161);
+            this.zahl6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl6.Name = "zahl6";
+            this.zahl6.Size = new System.Drawing.Size(67, 34);
+            this.zahl6.TabIndex = 7;
+            this.zahl6.Text = "6";
+            this.zahl6.UseVisualStyleBackColor = true;
+            this.zahl6.Click += new System.EventHandler(this.zahl6_Click);
+            // 
+            // zahl7
+            // 
+            this.zahl7.Location = new System.Drawing.Point(139, 214);
+            this.zahl7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl7.Name = "zahl7";
+            this.zahl7.Size = new System.Drawing.Size(67, 34);
+            this.zahl7.TabIndex = 8;
+            this.zahl7.Text = "7";
+            this.zahl7.UseVisualStyleBackColor = true;
+            this.zahl7.Click += new System.EventHandler(this.zahl7_Click);
+            // 
+            // zahl8
+            // 
+            this.zahl8.Location = new System.Drawing.Point(222, 214);
+            this.zahl8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl8.Name = "zahl8";
+            this.zahl8.Size = new System.Drawing.Size(67, 34);
+            this.zahl8.TabIndex = 9;
+            this.zahl8.Text = "8";
+            this.zahl8.UseVisualStyleBackColor = true;
+            this.zahl8.Click += new System.EventHandler(this.zahl8_Click);
+            // 
+            // zahl9
+            // 
+            this.zahl9.Location = new System.Drawing.Point(309, 214);
+            this.zahl9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl9.Name = "zahl9";
+            this.zahl9.Size = new System.Drawing.Size(67, 34);
+            this.zahl9.TabIndex = 10;
+            this.zahl9.Text = "9";
+            this.zahl9.UseVisualStyleBackColor = true;
+            this.zahl9.Click += new System.EventHandler(this.zahl9_Click);
+            // 
+            // pluszeichen
+            // 
+            this.pluszeichen.Location = new System.Drawing.Point(400, 110);
+            this.pluszeichen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pluszeichen.Name = "pluszeichen";
+            this.pluszeichen.Size = new System.Drawing.Size(67, 34);
+            this.pluszeichen.TabIndex = 11;
+            this.pluszeichen.Text = "+";
+            this.pluszeichen.UseVisualStyleBackColor = true;
+            this.pluszeichen.Click += new System.EventHandler(this.pluszeichen_Click);
+            // 
+            // minuszeichen
+            // 
+            this.minuszeichen.Location = new System.Drawing.Point(400, 163);
+            this.minuszeichen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.minuszeichen.Name = "minuszeichen";
+            this.minuszeichen.Size = new System.Drawing.Size(67, 34);
+            this.minuszeichen.TabIndex = 13;
+            this.minuszeichen.Text = "-";
+            this.minuszeichen.UseVisualStyleBackColor = true;
+            this.minuszeichen.Click += new System.EventHandler(this.minuszeichen_Click);
+            // 
+            // geteiltzeichen
+            // 
+            this.geteiltzeichen.Location = new System.Drawing.Point(400, 214);
+            this.geteiltzeichen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.geteiltzeichen.Name = "geteiltzeichen";
+            this.geteiltzeichen.Size = new System.Drawing.Size(67, 34);
+            this.geteiltzeichen.TabIndex = 14;
+            this.geteiltzeichen.Text = "/";
+            this.geteiltzeichen.UseVisualStyleBackColor = true;
+            this.geteiltzeichen.Click += new System.EventHandler(this.geteiltzeichen_Click);
+            // 
+            // malzeichen
+            // 
+            this.malzeichen.Location = new System.Drawing.Point(400, 266);
+            this.malzeichen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.malzeichen.Name = "malzeichen";
+            this.malzeichen.Size = new System.Drawing.Size(67, 34);
+            this.malzeichen.TabIndex = 15;
+            this.malzeichen.Text = "*";
+            this.malzeichen.UseVisualStyleBackColor = true;
+            this.malzeichen.Click += new System.EventHandler(this.malzeichen_Click);
+            // 
+            // resetknopf
+            // 
+            this.resetknopf.Location = new System.Drawing.Point(497, 110);
+            this.resetknopf.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.resetknopf.Name = "resetknopf";
+            this.resetknopf.Size = new System.Drawing.Size(67, 34);
+            this.resetknopf.TabIndex = 16;
+            this.resetknopf.Text = "reset";
+            this.resetknopf.UseVisualStyleBackColor = true;
+            this.resetknopf.Click += new System.EventHandler(this.resetknopf_Click);
+            // 
+            // zahl0
+            // 
+            this.zahl0.Location = new System.Drawing.Point(222, 266);
+            this.zahl0.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.zahl0.Name = "zahl0";
+            this.zahl0.Size = new System.Drawing.Size(67, 34);
+            this.zahl0.TabIndex = 17;
+            this.zahl0.Text = "0";
+            this.zahl0.UseVisualStyleBackColor = true;
+            this.zahl0.Click += new System.EventHandler(this.zahl0_Click);
+            // 
+            // gleichzeichen
+            // 
+            this.gleichzeichen.Location = new System.Drawing.Point(309, 266);
+            this.gleichzeichen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gleichzeichen.Name = "gleichzeichen";
+            this.gleichzeichen.Size = new System.Drawing.Size(67, 34);
+            this.gleichzeichen.TabIndex = 18;
+            this.gleichzeichen.Text = "=";
+            this.gleichzeichen.UseVisualStyleBackColor = true;
+            this.gleichzeichen.Click += new System.EventHandler(this.gleichzeichen_Click);
+            // 
+            // textFuerZwischenErgebnis
+            // 
+            this.textFuerZwischenErgebnis.Enabled = false;
+            this.textFuerZwischenErgebnis.Location = new System.Drawing.Point(222, 74);
+            this.textFuerZwischenErgebnis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textFuerZwischenErgebnis.Name = "textFuerZwischenErgebnis";
+            this.textFuerZwischenErgebnis.Size = new System.Drawing.Size(270, 22);
+            this.textFuerZwischenErgebnis.TabIndex = 19;
+            // 
+            // labelFuerZwischenergebnis
+            // 
+            this.labelFuerZwischenergebnis.AutoSize = true;
+            this.labelFuerZwischenergebnis.Location = new System.Drawing.Point(100, 74);
+            this.labelFuerZwischenergebnis.Name = "labelFuerZwischenergebnis";
+            this.labelFuerZwischenergebnis.Size = new System.Drawing.Size(116, 16);
+            this.labelFuerZwischenergebnis.TabIndex = 20;
+            this.labelFuerZwischenergebnis.Text = "Zwischenergebnis";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Ergebnis";
+            // 
+            // TaschenrechnerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(648, 360);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelFuerZwischenergebnis);
+            this.Controls.Add(this.textFuerZwischenErgebnis);
+            this.Controls.Add(this.gleichzeichen);
+            this.Controls.Add(this.zahl0);
+            this.Controls.Add(this.resetknopf);
+            this.Controls.Add(this.malzeichen);
+            this.Controls.Add(this.geteiltzeichen);
+            this.Controls.Add(this.minuszeichen);
+            this.Controls.Add(this.pluszeichen);
+            this.Controls.Add(this.zahl9);
+            this.Controls.Add(this.zahl8);
+            this.Controls.Add(this.zahl7);
+            this.Controls.Add(this.zahl6);
+            this.Controls.Add(this.zahl5);
+            this.Controls.Add(this.zahl4);
+            this.Controls.Add(this.zahl3);
+            this.Controls.Add(this.zahl1);
+            this.Controls.Add(this.zahl2);
+            this.Controls.Add(this.textFuerZahlen);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "TaschenrechnerForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textFuerZahlen;
+        private System.Windows.Forms.Button zahl2;
+        private System.Windows.Forms.Button zahl1;
+        private System.Windows.Forms.Button zahl3;
+        private System.Windows.Forms.Button zahl4;
+        private System.Windows.Forms.Button zahl5;
+        private System.Windows.Forms.Button zahl6;
+        private System.Windows.Forms.Button zahl7;
+        private System.Windows.Forms.Button zahl8;
+        private System.Windows.Forms.Button zahl9;
+        private System.Windows.Forms.Button pluszeichen;
+        private System.Windows.Forms.Button minuszeichen;
+        private System.Windows.Forms.Button geteiltzeichen;
+        private System.Windows.Forms.Button malzeichen;
+        private System.Windows.Forms.Button resetknopf;
+        private System.Windows.Forms.Button zahl0;
+        private System.Windows.Forms.Button gleichzeichen;
+        private System.Windows.Forms.TextBox textFuerZwischenErgebnis;
+        private System.Windows.Forms.Label labelFuerZwischenergebnis;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Taschenrechner/TaschenrechnerForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+
+    public partial class TaschenrechnerForm : Form
+    {
+        string letzteOperation = null;
+        double? letzeZahl = null;
+        bool textHatErgebnis = false;
+        public TaschenrechnerForm()
+        {
+            InitializeComponent();
+        }
+        private void OperatorButton(string Op)
+        {
+            if (this.textFuerZahlen.Text == "")
+            {
+
+            }
+            else
+            {
+                if (this.letzeZahl != null)
+                {
+                    this.gleichzeichen_Click(null, null);
+                }
+                this.letzeZahl = Convert.ToDouble(this.textFuerZahlen.Text);
+                this.textFuerZahlen.Text = "";
+                this.textFuerZwischenErgebnis.Text = Convert.ToString(letzeZahl) + " " + Op;
+                this.letzteOperation = Op;
+            }
+
+        }
+
+
+        private void zahl_Click(string zahl)
+        {
+            if (this.textHatErgebnis == true)
+            {
+                if (this.textFuerZahlen.Text != "")
+                {
+                    this.letzeZahl = Convert.ToDouble(this.textFuerZahlen.Text);
+                }
+                this.textFuerZahlen.Text = "";
+                this.textHatErgebnis = false;
+            }
+            this.textFuerZahlen.Text = this.textFuerZahlen.Text + zahl;
+        }
+
+        private void zahl1_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("1");
+        }
+
+        private void zahl2_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("2");
+        }
+
+        private void zahl3_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("3");
+        }
+
+        private void zahl4_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("4");
+        }
+
+        private void zahl5_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("5"); ;
+        }
+
+        private void zahl6_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("6");
+        }
+
+        private void zahl7_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("7");
+        }
+
+        private void zahl8_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("8");
+        }
+
+        private void zahl9_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("9");
+        }
+
+        private void zahl0_Click(object sender, EventArgs e)
+        {
+            this.zahl_Click("0");
+        }
+
+
+
+        private void minuszeichen_Click(object sender, EventArgs e)
+        {
+            this.OperatorButton("-");
+        }
+
+        private void gleichzeichen_Click(object sender, EventArgs e)
+        {
+            if (this.textFuerZahlen.Text == "")
+            {
+                return;
+            }
+            double? zahlvonText = Convert.ToDouble(this.textFuerZahlen.Text);
+            if (this.letzteOperation == "+")
+            {
+                double? ErgebnisPlus = letzeZahl + zahlvonText;
+                this.textFuerZahlen.Text = ErgebnisPlus.ToString();
+            }
+            else if (this.letzteOperation == "-")
+            {
+                double? ErgebnisMinus = (letzeZahl - zahlvonText);
+                this.textFuerZahlen.Text = ErgebnisMinus.ToString();
+            }
+            else if (this.letzteOperation == "*")
+            {
+                double? ErgebnisMal = (letzeZahl * zahlvonText);
+                this.textFuerZahlen.Text = ErgebnisMal.ToString();
+            }
+            else if (this.letzteOperation == "/")
+            {
+                double? ErgebnisGeteilt = (letzeZahl + zahlvonText);
+                this.textFuerZahlen.Text = ErgebnisGeteilt.ToString();
+            }
+            this.letzeZahl = null;
+            this.letzteOperation = null;
+            this.textHatErgebnis = true;
+            this.textFuerZwischenErgebnis.Text = this.textFuerZahlen.Text;
+
+        }
+
+        private void pluszeichen_Click(object sender, EventArgs e)
+        {
+            this.OperatorButton("+");
+        }
+
+        private void geteiltzeichen_Click(object sender, EventArgs e)
+        {
+            this.OperatorButton("/");
+        }
+
+        private void malzeichen_Click(object sender, EventArgs e)
+        {
+            this.OperatorButton("*");
+        }
+
+        private void resetknopf_Click(object sender, EventArgs e)
+        {
+            this.textFuerZahlen.Text = "";
+            this.textFuerZwischenErgebnis.Text = "";
+            this.letzeZahl = null;
+            this.letzteOperation = null;
+        }
+    }
+}
